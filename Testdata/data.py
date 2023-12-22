21f8d3a4a32bf9694c4a4f751820b45757c35532
Esvararajaa <142655605+Esvararajaa@users.noreply.github.com>
2023-12-23 00:07:09 +0530

    Add files via upload

---
+++ Testdata/data.py
@@ -0,0 +1,12 @@
+class Dforgot:
+    user = 'Admin'
+
+
+class Data:
+    url = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    username01 = "Admin"
+    password01 = "admin123"
+    menu = ['Admin', 'PIM', 'Leave', 'Time', 'Recruitment', 'My Info', 'Performance', 'Dashboard', 'Directory', 'Maintenance', 'Claim', 'Buzz']
+    nav = ['User Management', 'Job', 'Organization', 'Qualifications', 'Nationalities', 'Corporate Branding', 'Configuration']
+    com_nav = []
+    com_menu = []
